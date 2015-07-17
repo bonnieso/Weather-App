fa3676708608e9193e2fea12984f446f3898746f
skl92 <stanleykliu92@gmail.com>
2015-07-17 01:29:01 -0700

    integrate backend

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var session = require("express-session");
+
+mongoose.connect(process.env.MONGOLAB_URI);
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -12,7 +17,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -20,6 +25,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session({ secret: 'revengeofthenerds', resave: true, saveUninitialized: true })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+require('./config/passport')(passport);
+require('./routes/passport.js')(app, passport);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
